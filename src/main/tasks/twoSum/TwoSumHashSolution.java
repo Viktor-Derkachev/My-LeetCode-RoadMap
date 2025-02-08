8201efd2fe948d0f1febf8e021822b7b051282c8
Viktor Derkachev <viktor.der@neo.gg>
2025-02-08 21:49:46 +0100

    #MergeTwoSortedListSolution: MergeTwoSortedListSolution solution

--- src/main/tasks/twoSum/TwoSumHashSolution.java
+++ src/main/tasks/twoSum/TwoSumHashSolution.java
@@ -1,6 +1,8 @@
-package main.arrayshashing.twoSum;
+package main.tasks.twoSum;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * <a href="https://leetcode.com/problems/two-sum/">...</a>
@@ -32,7 +34,8 @@ import java.util.Arrays;
  * -109 <= target <= 109
  * Only one valid answer exists.
  */
-public class TwoSumSolution {
+public class TwoSumHashSolution {
+
     public static void main(String[] args) {
         int[] nums1 = new int[]{2, 7, 11, 15};
         int target1 = 9;
@@ -50,18 +53,15 @@ public class TwoSumSolution {
     }
 
     private static int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for (int i = 0; i < nums.length - 1; i++) {
-            for (int j = i + 1; j < nums.length; j++) {
-                if ((target - nums[i]) == nums[j]) {
-                    result[0] = i;
-                    result[1] = j;
-                    break;
-                }
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[]{map.get(complement), i};
             }
+            map.put(nums[i], i);
         }
-
-        return result;
+        return null;
     }
 }
